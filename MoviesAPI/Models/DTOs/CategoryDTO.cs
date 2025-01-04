c2bb878f23c14d67ac5c678078b213dd34acf7ae
Esmaeil Sheikhi <es.sheikhi@gmail.com>
2025-01-04 12:43:32 +0330

    Adding Category model and DTO

---
+++ MoviesAPI/Models/DTOs/CategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "{0} is required!")]
+        [MaxLength(10, ErrorMessage = "Max length is 10 characters")]
+        public string Name { get; set; } = default!;
+        public DateTime DateOfCreation { get; set; }
+    }
+}
---
+++ MoviesAPI/Models/DTOs/CreateCategoryDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models.DTOs
+{
+    public class CreateCategoryDTO
+    {
+        [Required(ErrorMessage = "{0} is required!")]
+        [MaxLength(10, ErrorMessage = "Max length is 10 characters")]
+        public string Name { get; set; } = default!;
+    }
+}
